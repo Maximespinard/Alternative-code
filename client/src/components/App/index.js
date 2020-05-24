b6ef332332e25c638e798aeb5a305a101be286e6
Chris <chris@MacBook-Air-de-Chris.local>
2020-05-24 12:12:38 +0200

    header

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./App.css";
+import Home from "../Home";
 
 function App() {
   return (
     <div className="App">
-      Alternative Code
+      <Home />
     </div>
   );
 }
