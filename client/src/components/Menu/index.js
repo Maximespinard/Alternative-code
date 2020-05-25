811f777d07e87a66fb2a293072461866771dd524
Maximespinard <maxiime.spinard@gmail.com>
2020-05-25 11:47:56 +0200

    barbare

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,35 +1,57 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/alternative.png";
+
 import "./Menu.css";
 
-const Menu = () => {
+const Header = () => {
+  const [burger, setBurger] = useState(false);
+
+  const handleClick = () => {
+    setBurger(!burger);
+  };
   return (
-    <Navbar className="menu" expand="lg">
-      <Navbar.Brand href="#home">
-        <img className="menu_logo" src={logo} alt="alternative code" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <header className="menu">
+        <div className=" d-flex justify-content-between ">
+          <div className=" image_menu-wrapper d-none d-md-block m-4">
+            <img src={logo} alt="alternative-code"/>
+          </div>
+          <div className="justify-content-end d-none d-md-flex menu_links">
+            <Link to="" className="menu_link">
+              <p>Home</p>
+            </Link>
+            <Link to="" className="menu_link">
+              <p>About</p>
+            </Link>
+            <Link to="" className="menu_link">
+              <p>Démo</p>
+            </Link>
+            <Link to="" className="menu_link">
+              <p>Contact</p>
+            </Link>
+          </div>
+        </div>
+        <div className="d-md-none d-flex justify-content-between i_wrapper">
+          <Link className="menu_i--link mt-2" to="/">
+            <img src={logo} alt="alternative-code"/>
+          </Link>
+          <i className="fas fa-bars" onClick={handleClick}></i>
+        </div>
+      <div className="d-none d-sm-block d-md-none menu_burger">
+        {burger && (
+          <div className="menu_burger--content">
+            <div>Home</div>
+            <div>About</div>
+            <div>Démo</div>
+            <div>Contact</div>
+          </div>
+        )}
+      </div>
+      </header>
+    </div>
   );
 };
 
-export default Menu;
+export default Header;
