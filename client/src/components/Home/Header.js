984ee84c2d0b8b9e2c1893b854bf50f81dd13e9f
Chris <chris@MacBook-Air-de-Chris.local>
2020-05-25 08:43:21 +0200

    header construction

--- client/src/components/Home/Header.js
+++ client/src/components/Home/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="header d-md-flex">
+      <div className="header__left">
+        <Typewriter
+          options={{
+            cursor: "",
+            strings: ["<h1>Hello World</h1>", "<h1>Ca va ou quoi ?</h1>"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+      <div className="header__right d-none d-md-block">right</div>
+    </div>
+  );
 };
 
 export default Header;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import About from "./About";
+import "./Home.css";
 
 const Home = () => {
   return (
