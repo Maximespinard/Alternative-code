0da9985084ab1496e76543c7211d40d29f3b8fe7
Maximespinard <maxiime.spinard@gmail.com>
2020-05-26 18:30:44 +0200

    s

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import img from "../../assets/images/social-media.jpg";
 
 const About = () => {
   return (
@@ -6,18 +7,24 @@ const About = () => {
       <div>
         <h2>Qui sommes nous ?</h2>
       </div>
-      <div className="col-lg-8">
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+      <div className="row m-3">
+        <div className="col-lg-5 about_wrapper">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button>Voir ce qu'on peux faire</button>
+        </div>
+        <div className="col-lg-6 d-none d-md-block">
+          <img src={img} alt="creativity" />
+        </div>
       </div>
     </div>
   );
