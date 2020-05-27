036cb77c6dd5d716bfdf467e954a16fbf0e9d474
Chris <chris@MacBook-Air-de-Chris.local>
2020-05-27 10:50:27 +0200

    sticky menu et restyling

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./Contact.css";
+import Menu from "../Menu";
+import Footer from "../Footer";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Fragment>
+      <Menu />
+      <div className="contact__wrapper">
+        <h1>Contact</h1>
+        <form className="col-md-5">
+          <input type="text" />
+          <input type="text" />
+          <button>Envoyer</button>
+        </form>
+      </div>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default Contact;
