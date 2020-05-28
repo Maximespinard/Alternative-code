12a604b154bd6e53e921f63b432f86f4f8603380
Chris <chris@MacBook-Air-de-Chris.local>
2020-05-28 09:25:16 +0200

    style contact

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -4,27 +4,23 @@ import Menu from "../Menu";
 import Footer from "../Footer";
 
 const Contact = () => {
-  const [bg, setBg] = useState("");
-  const getHour = new Date();
-  useLayoutEffect(() => {
-    if (getHour.getHours() <= 8 && getHour.getHours() >= 19) {
-      setBg(
-        "https://img.aws.la-croix.com/2020/01/20/1201072851/fenetre-Ciel_1_1399_933.jpg"
-      );
-    } else {
-      setBg(
-        " https://www.numerama.com/content/uploads/2019/11/etoiles-ciel-nuages-nuit-astronomie.jpg"
-      );
-    }
-  });
   return (
     <Fragment>
       <Menu />
-      <div
-        style={{ backgroundImage: `url(${bg})` }}
-        className="contact__wrapper"
-      >
+      <div className="contact__wrapper">
+        <div className="contact__infos d-md-flex">
+          <div className="col-md-3">
+            Mail <i className="far fa-envelope"></i>
+          </div>
+          <div className="col-md-3">
+            Téléphone <i className="fas fa-phone"></i>
+          </div>
+          <div className="col-md-3">
+            Prendre rendez-vous <i className="far fa-calendar-alt"></i>
+          </div>
+        </div>
         <form className="col-md-5">
+          <h3>Parlez-nous de votre projet</h3>
           <input name="name" type="text" placeholder="Votre nom" />
           <input name="email" type="text" placeholder="Votre email" />
           <textarea
@@ -33,6 +29,10 @@ const Contact = () => {
             rows="10"
             placeholder="Votre message"
           ></textarea>
+          <small>
+            En soumettant ce formulaire j'accepte que mes données soient utilisé
+            uniquement pour me répondre
+          </small>
           <button>Envoyer</button>
         </form>
       </div>
